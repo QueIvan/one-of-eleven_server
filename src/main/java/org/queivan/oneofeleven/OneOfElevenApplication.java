b3ca8d877e8f689346ba455552409fb4dfe6526e
Jan Szewczyk <36161455+QueIvan@users.noreply.github.com>
2022-05-27 11:17:20 +0200

    [Init] Project skeleton and couple dependency addition

---
+++ src/main/java/org/queivan/oneofeleven/OneOfElevenApplication.java
@@ -0,0 +1,13 @@
+package org.queivan.oneofeleven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneOfElevenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneOfElevenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/queivan/oneofeleven/OneOfElevenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.queivan.oneofeleven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneOfElevenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
